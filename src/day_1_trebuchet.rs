4d50b3887a669cf6d3bd9d5a3e832b515231055b
matzxrr <matzxrr@users.noreply.github.com>
2023-12-03 00:59:03 -0700

    feat: finish day 1 part 1

--- src/day_1_trebuchet.rs
+++ src/day_1_trebuchet.rs
@@ -1,8 +1,30 @@
-pub mod day_1 {
-    pub fn star_1(input: &str) -> i32 {
-        println!("{}", input);
-        0
+pub fn exec_star_1() -> i32 {
+    let input = include_str!("assets/star_1_input.txt");
+    star_1(input)
+}
+
+fn star_1(input: &str) -> i32 {
+    let lines = input.lines();
+    let mut total = 0;
+    for line in lines {
+        let mut result = String::new();
+        for char in line.chars() {
+            if char.is_numeric() {
+                result.push(char);
+                break;
+            }
+        }
+        let mut iter = line.chars();
+        while let Some(char) = iter.next_back() {
+            if char.is_numeric() {
+                result.push(char);
+                break;
+            }
+        }
+        println!("{}", result);
+        total += result.parse::<i32>().unwrap();
     }
+    total
 }
 
 #[cfg(test)]
@@ -12,7 +34,7 @@ mod day_1_test {
     #[test]
     fn test_star_1() {
         let input = include_str!("assets/star_1_test.txt");
-        let result = day_1::star_1(input);
-        assert_eq!(result, 77);
+        let result = star_1(input);
+        assert_eq!(result, 142);
     }
 }
