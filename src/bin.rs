9cf2a21ff955a970f044a40195c9552638d648df
matzxrr <matzxrr@users.noreply.github.com>
2023-12-03 21:06:40 -0700

    feat: remove sub command from cli

--- src/bin.rs
+++ src/bin.rs
@@ -1,5 +1,3 @@
-use core::panic;
-
 use clap::{Arg, ArgAction, ArgGroup, Command};
 use solutions::day_1_trebuchet;
 
@@ -7,33 +5,26 @@ fn cli() -> Command {
     Command::new("advent23")
         .about("Advent of Code 2023 Answer CLI")
         .author("matzxrr")
-        .subcommand_required(true)
-        .subcommand(
-            Command::new("answer")
-                .short_flag('a')
-                .long_flag("answer")
-                .about("Get the answer to a problem")
-                .group(ArgGroup::new("ans").required(true).multiple(false))
-                .arg(
-                    Arg::new("day")
-                        .short('d')
-                        .long("day")
-                        .help("Day you want the answers for")
-                        .group("ans")
-                        .conflicts_with("star")
-                        .action(ArgAction::Set)
-                        .num_args(1..),
-                )
-                .arg(
-                    Arg::new("star")
-                        .short('s')
-                        .long("star")
-                        .help("Star you want the answer for")
-                        .group("ans")
-                        .conflicts_with("day")
-                        .action(ArgAction::Set)
-                        .num_args(1..),
-                ),
+        .group(ArgGroup::new("ans").required(true).multiple(false))
+        .arg(
+            Arg::new("day")
+                .short('d')
+                .long("day")
+                .help("Day you want the answers for")
+                .group("ans")
+                .conflicts_with("star")
+                .action(ArgAction::Set)
+                .num_args(1..),
+        )
+        .arg(
+            Arg::new("star")
+                .short('s')
+                .long("star")
+                .help("Star you want the answer for")
+                .group("ans")
+                .conflicts_with("day")
+                .action(ArgAction::Set)
+                .num_args(1..),
         )
 }
 
@@ -42,33 +33,23 @@ fn get_day_answers(day: &str) {
 }
 
 fn get_star_answer(star: &str) {
-    print!("Answer for star '{}' => ", star);
     match star {
         "1" => {
             let answer = day_1_trebuchet::exec_star_1();
-            println!("{}", answer);
+            println!("Answer for star '{}' => {}", star, answer);
         }
-        _ => panic!("Unknown star"),
+        _ => eprintln!("Unknown star '{}'", star),
     }
 }
 
 fn main() {
     let matches = cli().get_matches();
-    match matches.subcommand() {
-        Some(("answer", answer_matches)) => {
-            if answer_matches.contains_id("day") {
-                let day = answer_matches
-                    .get_one::<String>("day")
-                    .expect("contains_id");
-                get_day_answers(day);
-            }
-            if answer_matches.contains_id("star") {
-                let star = answer_matches
-                    .get_one::<String>("star")
-                    .expect("contains_id");
-                get_star_answer(star);
-            }
-        }
-        _ => unreachable!(),
+    if matches.contains_id("day") {
+        let day = matches.get_one::<String>("day").expect("contains_id");
+        get_day_answers(day);
+    }
+    if matches.contains_id("star") {
+        let star = matches.get_one::<String>("star").expect("contains_id");
+        get_star_answer(star);
     }
 }
