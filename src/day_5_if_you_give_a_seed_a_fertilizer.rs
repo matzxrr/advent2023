5f390b28e8842feaacb582f911ffc12966764d57
matzxrr <matzxrr@users.noreply.github.com>
2023-12-06 01:04:15 -0700

    feat: starting writing a parser for day 5

--- src/day_5_if_you_give_a_seed_a_fertilizer.rs
+++ src/day_5_if_you_give_a_seed_a_fertilizer.rs
@@ -1,11 +1,61 @@
+#![allow(dead_code)]
+#![allow(unused_variables)]
+
 const INPUT: &str = include_str!("assets/day_5_input_1.txt");
 
 pub fn exec_star_9() -> i32 {
     star_9(INPUT)
 }
 
+trait Header {
+    const HEADER: &'static str;
+}
+
+#[derive(Debug)]
+struct Almanac {
+    seeds: Vec<i32>,
+}
+
+impl Almanac {
+    fn new() -> Self {
+        Self { seeds: vec![] }
+    }
+    fn parse_group(&mut self, group: &[&str]) {
+        if group[0].starts_with("seeds:") {
+            self.parse_seeds(group);
+        }
+    }
+    fn parse_seeds(&mut self, input: &[&str]) {
+        self.seeds = input[0]
+            .split_once(':')
+            .unwrap()
+            .1
+            .trim()
+            .split(' ')
+            .map(|n| n.parse::<i32>().unwrap())
+            .collect();
+    }
+}
+
 fn star_9(input: &str) -> i32 {
-    todo!()
+    let lines = input.lines();
+    let groups = lines.fold(vec![vec![]], |mut acc, value| {
+        if value.is_empty() {
+            acc.push(vec![]);
+        } else {
+            let index = acc.len() - 1;
+            acc[index].push(value);
+        }
+        acc
+    });
+
+    let mut almanac = Almanac::new();
+    for group in groups.iter() {
+        almanac.parse_group(group);
+    }
+
+    dbg!(&almanac);
+    0
 }
 
 #[cfg(test)]
