5b62aa3778b487a1cbd67a50cdc1642bd5ab2425
matzxrr <matzxrr@users.noreply.github.com>
2023-12-07 02:01:07 -0700

    feat: finish day 5 star 9

--- src/day_5_if_you_give_a_seed_a_fertilizer.rs
+++ src/day_5_if_you_give_a_seed_a_fertilizer.rs
@@ -1,6 +1,3 @@
-#![allow(dead_code)]
-#![allow(unused_variables)]
-
 const INPUT: &str = include_str!("assets/day_5_input_1.txt");
 
 pub fn exec_star_9() -> i32 {
@@ -12,17 +9,71 @@ trait Header {
 }
 
 #[derive(Debug)]
+struct Mapping {
+    destination_start: u64,
+    source_start: u64,
+    range_length: u64,
+}
+
+impl Mapping {
+    fn new(start: u64, source: u64, range: u64) -> Self {
+        Self {
+            destination_start: start,
+            source_start: source,
+            range_length: range,
+        }
+    }
+}
+
+#[derive(Debug)]
 struct Almanac {
-    seeds: Vec<i32>,
+    seeds: Vec<u64>,
+    seed_to_soil: Vec<Mapping>,
+    soil_to_fert: Vec<Mapping>,
+    fert_to_water: Vec<Mapping>,
+    water_to_light: Vec<Mapping>,
+    light_to_temp: Vec<Mapping>,
+    temp_to_humid: Vec<Mapping>,
+    humid_to_loc: Vec<Mapping>,
 }
 
 impl Almanac {
     fn new() -> Self {
-        Self { seeds: vec![] }
+        Self {
+            seeds: vec![],
+            seed_to_soil: vec![],
+            soil_to_fert: vec![],
+            fert_to_water: vec![],
+            water_to_light: vec![],
+            light_to_temp: vec![],
+            temp_to_humid: vec![],
+            humid_to_loc: vec![],
+        }
     }
     fn parse_group(&mut self, group: &[&str]) {
         if group[0].starts_with("seeds:") {
             self.parse_seeds(group);
+        }
+        if group[0].starts_with("seed-to-soil") {
+            self.seed_to_soil = Almanac::parse_mapping(group);
+        }
+        if group[0].starts_with("soil-to-fertilizer") {
+            self.soil_to_fert = Almanac::parse_mapping(group);
+        }
+        if group[0].starts_with("fertilizer-to-water") {
+            self.fert_to_water = Almanac::parse_mapping(group);
+        }
+        if group[0].starts_with("water-to-light") {
+            self.water_to_light = Almanac::parse_mapping(group);
+        }
+        if group[0].starts_with("light-to-temperature") {
+            self.light_to_temp = Almanac::parse_mapping(group);
+        }
+        if group[0].starts_with("temperature-to-humidity") {
+            self.temp_to_humid = Almanac::parse_mapping(group);
+        }
+        if group[0].starts_with("humidity-to-location") {
+            self.humid_to_loc = Almanac::parse_mapping(group);
         }
     }
     fn parse_seeds(&mut self, input: &[&str]) {
@@ -32,8 +83,34 @@ impl Almanac {
             .1
             .trim()
             .split(' ')
-            .map(|n| n.parse::<i32>().unwrap())
+            .map(|n| n.parse::<u64>().unwrap())
             .collect();
+    }
+    fn parse_mapping(input: &[&str]) -> Vec<Mapping> {
+        let mut lines = input.iter();
+        lines.next(); // Skip first line
+        lines
+            .map(|l| {
+                let s: Vec<_> = l
+                    .split(' ')
+                    .map(|n| n.trim())
+                    .filter(|n| !n.is_empty())
+                    .map(|n| n.parse::<u64>().unwrap())
+                    .collect();
+                s
+            })
+            .map(|n| Mapping::new(n[0], n[1], n[2]))
+            .collect::<Vec<_>>()
+    }
+    fn find_next(mappings: &Vec<Mapping>, start: u64) -> u64 {
+        for mapping in mappings {
+            if start >= mapping.source_start && start <= mapping.source_start + mapping.range_length
+            {
+                let offset = start - mapping.source_start;
+                return mapping.destination_start + offset;
+            }
+        }
+        start
     }
 }
 
@@ -54,8 +131,21 @@ fn star_9(input: &str) -> i32 {
         almanac.parse_group(group);
     }
 
-    dbg!(&almanac);
-    0
+    // seed and location
+    let mut closets_seed = (u64::MAX, u64::MAX);
+    for seed in almanac.seeds {
+        let soil = Almanac::find_next(&almanac.seed_to_soil, seed);
+        let fert = Almanac::find_next(&almanac.soil_to_fert, soil);
+        let water = Almanac::find_next(&almanac.fert_to_water, fert);
+        let light = Almanac::find_next(&almanac.water_to_light, water);
+        let temp = Almanac::find_next(&almanac.light_to_temp, light);
+        let humid = Almanac::find_next(&almanac.temp_to_humid, temp);
+        let loc = Almanac::find_next(&almanac.humid_to_loc, humid);
+        if loc < closets_seed.1 {
+            closets_seed = (seed, loc);
+        }
+    }
+    closets_seed.1 as i32
 }
 
 #[cfg(test)]
