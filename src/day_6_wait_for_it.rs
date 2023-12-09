857baae36dde02088a69622bddc15c8bc642e904
matzxrr <matzxrr@users.noreply.github.com>
2023-12-08 20:05:23 -0700

    feat: start solving day 6

--- src/day_6_wait_for_it.rs
+++ src/day_6_wait_for_it.rs
@@ -1,5 +1,51 @@
 const INPUT: &str = include_str!("assets/day_6_input_1.txt");
 
+struct Race {
+    time: i32,
+    distance: i32,
+}
+
+#[derive(Clone, Copy)]
+struct RacePermutation {
+    race_time_length: i32,
+    button_duration: i32,
+    distance_traveled: i32,
+}
+
+impl RacePermutation {
+    fn new(button_duration: i32, race_time_length: i32) -> Self {
+        let distance_traveled = button_duration * (race_time_length - button_duration);
+        Self {
+            race_time_length,
+            button_duration,
+            distance_traveled,
+        }
+    }
+}
+
+struct TimeCard {
+    races: Vec<Race>,
+}
+
+impl TimeCard {
+    fn get_race_permutations(race_time_length: i32) -> Vec<RacePermutation> {
+        let mut race_permutations: Vec<RacePermutation> = vec![];
+        for button_held_duration in 0..=race_time_length {
+            race_permutations.push(RacePermutation::new(button_held_duration, race_time_length));
+        }
+        race_permutations
+    }
+
+    fn get_winning_permutations(
+        race_permutations: &[RacePermutation],
+        minimum_distance: i32,
+    ) -> impl Iterator<Item = &RacePermutation> {
+        race_permutations
+            .iter()
+            .filter(move |r| r.distance_traveled > minimum_distance)
+    }
+}
+
 pub fn exec_star_11() -> i32 {
     star_11(INPUT)
 }
