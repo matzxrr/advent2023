678899fd65d32a20f0a79f0fa214510e8f347e4d
matzxrr <matzxrr@users.noreply.github.com>
2023-12-08 21:43:15 -0700

    feat: finish day 6 part 1

--- src/day_6_wait_for_it.rs
+++ src/day_6_wait_for_it.rs
@@ -1,11 +1,13 @@
 const INPUT: &str = include_str!("assets/day_6_input_1.txt");
 
+#[derive(Debug)]
 struct Race {
     time: i32,
     distance: i32,
 }
 
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, Debug)]
+#[allow(dead_code, unused)]
 struct RacePermutation {
     race_time_length: i32,
     button_duration: i32,
@@ -23,11 +25,31 @@ impl RacePermutation {
     }
 }
 
+#[derive(Debug)]
 struct TimeCard {
     races: Vec<Race>,
 }
 
 impl TimeCard {
+    fn new(input: &str) -> Self {
+        let mut parsed_lines = vec![];
+        for line in input.lines() {
+            let (_, right) = line.split_once(':').unwrap();
+            let times: Vec<_> = right
+                .split(' ')
+                .filter(|n| !n.is_empty())
+                .map(|n| n.parse::<i32>().unwrap())
+                .collect();
+            parsed_lines.push(times);
+        }
+        let races: Vec<Race> = (0..parsed_lines[0].len())
+            .map(|n| Race {
+                time: parsed_lines[0][n],
+                distance: parsed_lines[1][n],
+            })
+            .collect();
+        Self { races }
+    }
     fn get_race_permutations(race_time_length: i32) -> Vec<RacePermutation> {
         let mut race_permutations: Vec<RacePermutation> = vec![];
         for button_held_duration in 0..=race_time_length {
@@ -51,7 +73,15 @@ pub fn exec_star_11() -> i32 {
 }
 
 fn star_11(input: &str) -> i32 {
-    0
+    let time_card = TimeCard::new(input);
+    let mut result: i32 = 1;
+    for race in time_card.races {
+        let race_permutations = TimeCard::get_race_permutations(race.time);
+        let winning_permutations =
+            TimeCard::get_winning_permutations(&race_permutations, race.distance);
+        result *= winning_permutations.count() as i32;
+    }
+    result
 }
 
 #[cfg(test)]
