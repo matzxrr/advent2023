8240e690943c0a98c804c7791675ffd3e04ee325
matzxrr <matzxrr@users.noreply.github.com>
2023-12-19 22:27:37 -0700

    feat: parse input for day 12

--- src/day_12_hot_springs.rs
+++ src/day_12_hot_springs.rs
@@ -1,10 +1,36 @@
 const INPUT: &str = include_str!("assets/day_12_input_1.txt");
 
+#[derive(Debug)]
+struct Springs<'a> {
+    map: Vec<(&'a str, Vec<i32>)>,
+}
+
+impl<'a> From<&'a str> for Springs<'a> {
+    fn from(value: &'a str) -> Self {
+        let map = value
+            .lines()
+            .map(|l| {
+                let (left, right) = l.split_once(' ').unwrap();
+                (
+                    left,
+                    right
+                        .split(',')
+                        .map(|n| n.parse::<i32>().unwrap())
+                        .collect(),
+                )
+            })
+            .collect();
+        Self { map }
+    }
+}
+
 pub fn exec_star_23() -> i32 {
     star_23(INPUT)
 }
 
 fn star_23(input: &str) -> i32 {
+    let springs = Springs::from(input);
+    dbg!(&springs);
     0
 }
 
